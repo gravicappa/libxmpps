76cadbafa4d57392fdaa49eba8415bb7fa81f68c
Ramil Farkhshatov <ramil@gmx.co.uk>
2010-12-20 18:04:52 +0300

    Added prototypes to some pool functions.

--- pool.h
+++ pool.h
@@ -12,5 +12,9 @@ void pool_clean(struct pool *p);
 int pool_new_str(struct pool *p, const char *str);
 int pool_new_strn(struct pool *p, const char *str, int len);
 char *pool_ptr(struct pool *p, int h);
+
 int pool_state(struct pool *p);
 void pool_restore(struct pool *p, int mark);
+
+int pool_append_str(struct pool *p, int h, const char *str);
+int pool_append_char(struct pool *p, int h, char c);
